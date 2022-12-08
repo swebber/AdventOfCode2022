6345f11abacd9a56da7b91948eb06b897b895a2d
R. Scott Webber <scott@teagan.org>
2022-12-07 22:10:46 -0500

    Update Program.cs

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,6 +1,12 @@
 ﻿// a,x == rock     == 1
 // b,y == paper    == 2
 // c,z == scissors == 3
+
+// Game Outcome
+// x == lose
+// y == draw
+// z == win
+
 // lose == 0
 // draw == 3
 // win  == 6
@@ -10,6 +16,12 @@ int totalScore = 0;
 foreach (string line in File.ReadLines(@"C:\Users\WebberS\source\repos\AdventOfCode2022\Day2\Day2\data.txt"))
 {
     var play = line.Split(" ");
+
+    // switch the move but keep everything else the same
+    string originalMove = play[1];
+    string newMove = DetermineMove(play);
+    play[1] = newMove;
+
     int score = Value(play[1]);
     int points = Points(play);
     int total = score + points;
@@ -23,6 +35,48 @@ Console.WriteLine($"Total: {totalScore}");
 
 public static partial class Program
 {
+    public static string DetermineMove(string[] play)
+    {
+        switch (play[1])
+        {
+            case "X": // lose
+                switch (play[0])
+                {
+                    case "A":
+                        return "Z";
+                    case "B":
+                        return "X";
+                    case "C":
+                        return "Y";
+                }
+                break;
+            case "Y": // draw
+                switch (play[0])
+                {
+                    case "A":
+                        return "X";
+                    case "B":
+                        return "Y";
+                    case "C":
+                        return "Z";
+                }
+                break;
+            default: // win
+                switch (play[0])
+                {
+                    case "A":
+                        return "Y";
+                    case "B":
+                        return "Z";
+                    case "C":
+                        return "X";
+                }
+                break;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(play));
+    }
+
     public static int Points(string[] play)
     {
         string pair = $"{play[1]}{play[0]}";
