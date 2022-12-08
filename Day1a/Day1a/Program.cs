0c37ba330bd48b621ed9bc5d3d3c5eb74d8463a0
R. Scott Webber <swebber@thehwpgroup.com>
2022-12-07 20:58:04 -0500

    initial solution

--- Day1a/Day1a/Program.cs
+++ Day1a/Day1a/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int elf = 1;
+int maxElf = 0;
+int elfCalories = 0;
+int maxCalories = 0;
+
+foreach (string line in File.ReadLines(@"C:\Users\WebberS\source\repos\AdventOfCode2022\Day1a\Day1a\data.txt"))
+{
+    if (int.TryParse(line, out int calories))
+    {
+        elfCalories += calories;
+        if (elfCalories > maxCalories)
+        {
+            maxElf = elf;
+            maxCalories = elfCalories;
+        }
+
+        continue;
+    }
+
+    ++elf;
+    elfCalories = 0;
+}
+
+Console.WriteLine($"Max Elf: {maxElf}\nMax Calories: {maxCalories}");
