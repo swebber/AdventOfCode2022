701f32370b8472a81ca46bae5e8ce1158753188c
R. Scott Webber <scott@teagan.org>
2022-12-07 22:40:10 -0500

    Day 3 Part 1

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,17 @@
+﻿string pointsIndex = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int totalPriority = 0;
+
+foreach (string line in File.ReadLines(@"C:\Users\WebberS\source\repos\AdventOfCode2022\Day3\Day3\data.txt"))
+{
+    int lineLength = line.Length;
+    string compartment1 = line.Substring(0, lineLength / 2);
+    string compartment2 = line.Substring(lineLength / 2);
+
+    var sharedType = compartment1.Intersect(compartment2).First();
+    int priority = pointsIndex.IndexOf(sharedType);
+    totalPriority += priority;
+
+    Console.WriteLine($"{compartment1} - {compartment2} == {sharedType} / {priority}");
+}
+
+Console.WriteLine($"Total priority: {totalPriority}");
