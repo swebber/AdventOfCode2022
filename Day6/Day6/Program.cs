0187de0fef54ffb3747be55a879a197e01a9e763
R. Scott Webber <scott@teagan.org>
2022-12-09 08:21:20 -0500

    Day 6 Part 1

---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,17 @@
+﻿var line = File.ReadLines(@"C:\Users\WebberS\source\repos\AdventOfCode2022\Day6\Day6\data.txt").First();
+
+const int packetLength = 4;
+const int packetOffset = 3;
+int packetCount = line.Length - packetOffset;
+
+for (int i = 0; i < packetCount; i++)
+{
+    string packet = line.Substring(i, packetLength);
+    if (packet.Distinct().Count() == packetLength)
+    {
+        Console.WriteLine($"First market after character: {i + packetLength}");
+        return;
+    }
+}
+
+Console.WriteLine("No marker found.");
