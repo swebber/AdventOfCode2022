1c4862c1d09e49e1bd038830ed4193b102658029
R. Scott Webber <scott@teagan.org>
2022-12-08 20:51:33 -0500

    Day 4 Part 2

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -3,7 +3,7 @@
 foreach (string line in File.ReadLines(@"C:\Users\WebberS\source\repos\AdventOfCode2022\Day4\Day4\data.txt"))
 {
     var (pair1, pair2) = Pairs(line);
-    if (EitherContains(pair1, pair2))
+    if (Overlaps(pair1, pair2))
     {
         Console.WriteLine($"Contained pair: {pair1} - {pair2}");
         contain++;
@@ -25,6 +25,14 @@ public static partial class Program
         var pair2 = new Tuple<int, int>(int.Parse(pair[0]), int.Parse(pair[1]));
 
         return (pair1, pair2);
+    }
+
+    public static bool Overlaps(Tuple<int, int> p1, Tuple<int, int> p2)
+    {
+        if (p1.Item1 >= p2.Item1 && p1.Item1 <= p2.Item2) return true;
+        if (p1.Item2 >= p2.Item1 && p1.Item2 <= p2.Item2) return true;
+        if (p1.Item1 <= p2.Item1 && p1.Item2 >= p2.Item2) return true;
+        return false;
     }
 
     public static bool EitherContains(Tuple<int, int> p1, Tuple<int, int> p2)
