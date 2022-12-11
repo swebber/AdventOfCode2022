987fac32dabe9aa48c640f83a0703bd2e9f10076
R. Scott Webber <scott@teagan.org>
2022-12-10 20:48:13 -0500

    Day 9 Part 2

--- Day9/Day9/Processor.cs
+++ Day9/Day9/Processor.cs
@@ -5,8 +5,23 @@ namespace Day9;
 public class Processor
 {
     private string _fileName = @"C:\Users\WebberS\source\repos\AdventOfCode2022\Day9\Day9\data.txt";
+
+    private const int NumberOfKnots = 10;
+    private readonly int _numberOfTails = NumberOfKnots - 1;
+
     private readonly HeadLocation _headLocation = new();
-    private readonly TailLocation _tailLocation = new();
+    private readonly List<TailLocation> _tailLocations = new();
+
+    public Processor()
+    {
+        int numberOfTails = NumberOfKnots - 1;
+
+        for (int i = 0; i < numberOfTails; i++)
+        {
+            bool trackPosition = i + 1 == _numberOfTails;
+            _tailLocations.Add(new TailLocation((i + 1).ToString(), trackPosition));
+        }
+    }
 
     public void Run()
     {
@@ -16,10 +31,17 @@ public class Processor
             if (line.Split(' ') is not [var direction, var distance])
                 throw new ArgumentOutOfRangeException(nameof(line));
             MoveHead(direction, int.Parse(distance));
-            Console.WriteLine($"Move: {line}, Head ({_headLocation.X},{_headLocation.Y}), Tail ({_tailLocation.X},{_tailLocation.Y})");
+
+            Console.Write($"Move: {line}, Head ({_headLocation.X},{_headLocation.Y})");
+            foreach (var knot in _tailLocations)
+            {
+                Console.Write($", Tail {knot.Name} ({knot.X},{knot.Y})");
+            }
+            Console.WriteLine();
         }
 
-        Console.WriteLine($"\nNumber of Tail positions: {_tailLocation.NumberOfTailLocations}");
+        TailLocation lastKnot = _tailLocations.Last();
+        Console.WriteLine($"\nNumber of Tail positions: {lastKnot.NumberOfTailLocations}");
     }
 
     private void MoveHead(string direction, int distance)
@@ -27,25 +49,37 @@ public class Processor
         for (int i = 0; i < distance; i++)
         {
             _headLocation.Move(direction);
-            _tailLocation.Follow(_headLocation);
+
+            ILocation previousKnot  = _headLocation;
+            foreach (var tailLocation in _tailLocations)
+            {
+                tailLocation.Follow(previousKnot);
+                previousKnot = tailLocation;
+            }
         }
     }
 }
 
-public class TailLocation
+public class TailLocation : ILocation
 {
     public int X { get; set; } = 0;
     public int Y { get; set; } = 0;
 
+    public string Name { get; }
+    private readonly bool _trackPosition;
+    
     public List<Point> Points { get; set; } = new();
     public int NumberOfTailLocations => Points.Count;
 
-    public TailLocation()
+    public TailLocation(string name, bool trackPosition = false)
     {
-        Points.Add(new Point(X, Y));
+        Name = name;
+        _trackPosition = trackPosition;
+        if (_trackPosition)
+            Points.Add(new Point(X, Y));
     }
 
-    public void Follow(HeadLocation head)
+    public void Follow(ILocation head)
     {
         double a = Math.Pow(head.X - X, 2d);
         double b = Math.Pow(head.Y - Y, 2d);
@@ -66,12 +100,16 @@ public class TailLocation
         X += rowOffset;
         Y += colOffset;
 
-        var pointAfterMove = new Point(X, Y);
-        if (!Points.Contains(pointAfterMove)) Points.Add(pointAfterMove);
+        if (_trackPosition)
+        {
+            var pointAfterMove = new Point(X, Y);
+            if (!Points.Contains(pointAfterMove)) 
+                Points.Add(pointAfterMove);
+        }
     }
 }
 
-public class HeadLocation
+public class HeadLocation : ILocation
 {
     public int X { get; set; } = 0;
     public int Y { get; set; } = 0;
@@ -96,4 +134,10 @@ public class HeadLocation
                 throw new ArgumentOutOfRangeException(nameof(direction));
         }
     }
+}
+
+public interface ILocation
+{
+    public int X { get; set; }
+    public int Y { get; set; }
 }
